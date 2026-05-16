2823072a7358fe700fb88aa870f9ecea0bf233a0
Naphier Awalie <naphiera@gmail.com>
2026-05-16 13:05:55 +0800

    Update AssignedRidersByZone.tsx

--- dashboard/src/components/geofence/AssignedRidersByZone.tsx
+++ dashboard/src/components/geofence/AssignedRidersByZone.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ChevronDown } from 'lucide-react';
 import type { Zone, Rider, AttendanceLog } from '../../services/mockData';
 interface AssignedRidersByZoneProps {
