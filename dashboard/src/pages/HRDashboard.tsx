115c9df9fa482aead5ad09941753cc3923d651f0
Naphier Awalie <naphiera@gmail.com>
2026-05-17 00:30:34 +0800

    Update HRDashboard.tsx

--- dashboard/src/pages/HRDashboard.tsx
+++ dashboard/src/pages/HRDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BadgeCheck, ClipboardCheck, UserX, AlertCircle } from 'lucide-react';
 import { StatCard } from '../components/common/StatCard';
 import { useRealtimeLocation } from '../hooks/useRealtimeLocation';
